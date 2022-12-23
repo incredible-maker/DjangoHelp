e2af9352681a66af4ca7c526825fea8b51996425
JACKYZ0306 <jacky37583580@gmail.com>
2022-12-23 12:10:49 +0100

    New error on article, nothing show up

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Article
+from django.http import HttpResponse
+
+
 # Create your views here.
 
 # jacky is primary school student
@@ -7,3 +10,7 @@ def article_list(request):
     articles = Article.objects.all().order_by('date')
 
     return render(request, 'articles/article_list.html', {'articles': articles})
+
+
+def article_detail(request, slug):
+    return HttpResponse(slug)
