e264f0b31134d37c8e908d53f01b2c995e17ccca
JACKYZ0306 <jacky37583580@gmail.com>
2022-12-23 15:17:28 +0100

    error can't display solve

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -5,9 +5,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-# jacky is primary school student
+
 def article_list(request):
     articles = Article.objects.all().order_by('date')
+
 
     return render(request, 'articles/article_list.html', {'articles': articles})
 
